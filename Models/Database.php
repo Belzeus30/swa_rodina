03c19f6447c64d3f5176abc5bef61e1224a0665b
Plejrovec <stepanhruban@seznam.cz>
2023-03-27 12:44:28 +0200

    CRINGE

---
+++ Models/Database.php
@@ -0,0 +1,34 @@
+<?php
+    class Database {
+        
+        private $host;
+        private $db_name;
+        private $username;
+        private $password;
+        private $conn;
+
+        public function __construct($host, $db_name, $username , $password) {
+            $this->host = $host;
+            $this->db_name = $db_name;
+            $this->username = $username;
+            $this->password = $password;
+        }
+
+        public function connect() {
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+        public function query($sql) {
+            $this->conn->query($sql);
+        }
+
+    }
+
+
+ ?>
