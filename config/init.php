45eb318266486875d472b6c03cc15b5ebf9248b3
Plejrovec <stepanhruban@seznam.cz>
2023-03-29 10:30:11 +0200

    ano

---
+++ config/init.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once "../Models/Database.php";
+
+$db= new Database("b6tvkwyx0klomllwbt6l-mysql.services.clever-cloud.com","b6tvkwyx0klomllwbt6l","u2qzafc8q9vmh7sw","TvMU915nQqMY9qY8F1Q2");
+$Family = new Family($db->conn);
