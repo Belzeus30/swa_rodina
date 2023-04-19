cbe420cda334a389b63e166858acd7042054e005
Plejrovec <stepanhruban@seznam.cz>
2023-04-19 11:23:12 +0200

    pomoc

--- includes/footer.js
+++ includes/footer.js
@@ -1,16 +1,36 @@
  
-function Show_result(id) {
-    let data = {'id':id};
-    $.ajax({
-        url: "includes/family_details.php",
-        method: "post",
-        data: data,
-        success : function(){
-            //alert(1);
-            document.getElementById('family-detail').style.display = "show";
-            $('#family-detail').show();
-        },
-        error : function() {alert(error)} ,
-    })
+const familyDiv = document.getElementsByClassName('family');
 
+const familyDetails = document.getElementById('family-detail');
+
+Array.from(familyDiv).forEach(function(element) {
+    element.addEventListener('click', function() {
+        const familyId = this.dataset.familyId; 
+        familyDetails.classList.toggle('active');
+          data = {'id':familyId};
+        $.ajax({
+            url: 'parsers/get_family_details.php', 
+            type: 'POST',
+            data: data,
+            success: (response)=> {
+                $.ajax({
+                    type:'POST',
+                    url: 'index.php',
+                    data: {res: response},
+                    success: (data)=> {alert(data)}
+                })
+            },
+            error: ()=>{alert("error")}
+        })
+    });
+  });
+document.addEventListener('click', function(event) {
+if (event.target !== familyDetails && !familyDetails.contains(event.target)) {
+    familyDetails.classList.remove('active');
 }
+});
+  
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
