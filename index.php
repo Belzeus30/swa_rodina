305de4e0715ca569206ef9625366c8fa10fc32c2
Belzeus30 <120560948+Belzeus30@users.noreply.github.com>
2023-04-21 09:03:52 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="cs">
 <link rel="stylesheet" href="styles/style.css">
-<link rel="stylesheet" href="styles/index.css">
 
 <head>
 	<meta charset="UTF-8" />
@@ -19,23 +18,28 @@ $result = $Family->get_all();
 
 ?>
 
-<body>
-	<div class="main">
-		<h1>Rodina projekt</h1>
-		<button class="button" onclick=openForm(myForm)>Zapis</button>
-		<div class="family-list">
-			
-			<?php while ($row = $result->fetch(PDO::FETCH_ASSOC)) : ?>
-				<div class="family" data-family-id="<?= $row['ID'] ?>">
+<body>;	
+	<h1>Rodina projekt</h1>
+	<div class="family-list">
+
+		<?php while ($row = $result->fetch(PDO::FETCH_ASSOC)) : ?>
+			<div class="family" data-family-id="<?= $row['ID'] ?>">
+				<div>
 					<h4><?= $row["name"] ?></h4>
 					<p>Příjmy: <?= $row["prijmy"] ?></p>
-					<span> </span>
 					<p>Výdaje: <?= $row["vydaje"] ?></p>
 					<p>Cashflow: <?= $row["cashflow"] ?></p>
 					<p>Dluh: <?= $row["dluh"] ?></p>
 				</div>
-			<?php endwhile ?>
-		</div>
+				<div>
+					<button onclick="Edit_family()">Upravit</button>
+					<button onclick="Delete_family()">Smazat</button>
+				</div>
+			</div>
+		<?php endwhile ?>
+	</div>
+	<div class=center>
+		<button class="button" onclick=openForm(myForm)>Zapis</button>
 	</div>
 	<div class="inputbox">
 		<div class="column middle">
@@ -130,20 +134,7 @@ $result = $Family->get_all();
 			</div>
 		</div>
 	</div>
-	<div class="outputbox">
-		<div class="column middle">
-			<div class="table">
-				<h1 id="zapis">
-					Zůstatek
-				</h1>
-				<p>Váš zůstatek je (zustatek)</p>
-				<p>Váš dluh na kreditce činí(kdluh), Vače hypotéka činí(hdluh)</p>
-				<p>Pro úpravu přepište údaje a klikněte na tlačitko</p>
-				<p>if zustatek vetsi nez (6*(plat_1+plat_2)) then napad na investice else if (zustatek vetsi nez 0)
-					schov si penize, else zvedni prijmy a sniz vydaje </p>
-			</div>
-		</div>
-	</div>
+	
 </body>
 
 </html>
