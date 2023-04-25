b14267505a79d9de1bd2e0c942ab82cf0dcfc8fe
Plejrovec <stepanhruban@seznam.cz>
2023-04-25 10:29:12 +0200

    ne

--- includes/edit_form.js
+++ includes/edit_form.js
@@ -1,6 +1,8 @@
 const editbutton = document.querySelector('[data-enable-button]')
 const inputs = document.querySelectorAll('[data-form-input]')
 const editform = document.querySelector('[data-edit-form]')
+const editpopup = document.querySelector('[data-edit-popup]')
+const overlay = documet.querySelector('[data-edit-overlay]')
 
 
 editbutton.addEventListener("click", () => {
@@ -18,3 +20,19 @@ editbutton.addEventListener("click", () => {
     
 
 })
+
+const familyDiv = document.querySelectorAll('[data-family-id]');
+
+Array.from(familyDiv).forEach(function(e) {
+    e.addEventListener('click', function() {
+        const familyId = this.dataset.familyId; 
+          data = {'edit_id':familyId};
+        $.ajax({
+            url: 'includes/family_details.php', 
+            type: 'POST',
+            data: data,
+            success: (data)=>{
+                $('body').append(data);},
+            error: ()=>{alert("error")}
+        })
+    })})
--- includes/family_details.php
+++ includes/family_details.php
@@ -14,21 +14,123 @@ if (isset($_POST['edit_id'])):
 
     ?>
     <script src="includes/edit_form.js" defer></script>
-    <div id="family-detail">
+    <div id="family-detail" data-edit-overlay >
 
-        <div class="edit-popup">
+        <div class="edit-popup" data-edit-popup>
+            <div class="close" onclick="location.reload()">&#10006;</div>
             <form data-edit-form method="post" action="parsers/edit.php?id=<?= $selected_id ?>">
-                <h1 id="zapis">
-                    Zůstatek
-                </h1>
-                <div class="main-details">
-                    <p>Zůstatek:
+                <h3  id="zapis">
+                    Detail rodiny
+                </h3>
+                <div class="main-content">
+                    <div class="family-name">
+                        <label for="name">Jméno rodiny</label>
+                        <input name="name"  value="<?= $selected_family['name'] ?>" data-form-input disabled>
+                    </div>
+                    
+ 
+                    <div class="section">
+                        
+                        <div class="form-group">
+                            <label for="plat">1.plat (měsíčně):</label>
+                            <input name="plat" type="text"  value="<?= $selected_family['plat_1'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="2plat">2.plat (měsíčně):</label>
+                            <input name="2plat" type="text"  value="<?= $selected_family['plat_2'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="jedno_prijem">Jednorázové přijmy:</label>
+                            <input name="jedno_prijem" type="text" 
+                                value="<?= $selected_family['jedno_prijem'] ?>" data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="investice">Investice:</label>
+                            <input name="investice" type="text"  value="<?= $selected_family['investice'] ?>"
+                                data-form-input disabled>
+                        </div>
+                    </div>
+                    <!-- Vydaje -->
+
+                    <div class="section">
+                    
+                        <div class="form-group">
+                            <label for="najem">Nájem/Daň(měsíčně):</label>
+                            <input name="najem" type="text"  value="<?= $selected_family['najem'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="inkaso">Voda+Elektřina(za měsíc):</label>
+                            <input name="inkaso" type="text"  value="<?= $selected_family['inkaso'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="jidlo">Jídlo:</label>
+                            <input name="jidlo" type="text"  value="<?= $selected_family['jidlo'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="telekomunikace">Telekomunikace:</label>
+                            <input name="telekomunikace" type="text" 
+                                value="<?= $selected_family['telekomunikace'] ?>" data-form-input disabled>
+                        </div>
+                    </div>
+                    <!-- Vydaje zbytne -->
+                    <div class="section">
+                        <div class="form-group">
+                            <label for="konicky">Koníčky:</label>
+                            <input name="konicky" type="text"  value="<?= $selected_family['konicky'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="vylety">Výlet+zábava..:</label>
+                            <input name="vylety" type="text"  value="<?= $selected_family['vylety'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="vzdelani">Vzdělání:</label>
+                            <input name="vzdelani" type="text"  value="<?= $selected_family['vzdelani'] ?>"
+                                data-form-input disabled>
+                        </div>
+
+                    </div>
+                    <!-- Dluhy -->
+                    <div class="section">
+                    
+                        <div class="form-group">
+                            <label for="kcelkem">Hypotéka:</label>
+                            <input name="kcelkem" type="text"  value="<?= $selected_family['kcelkem'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="ksplatka">Splátka hypo:</label>
+                            <input name="ksplatka" type="text"  value="<?= $selected_family['ksplatka'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="hcelkem">Kreditka:</label>
+                            <input name="hcelkem" type="text"  value="<?= $selected_family['hcelkem'] ?>"
+                                data-form-input disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="hsplatkay">Splátka kreditky:</label>
+                            <input name="hsplatka" type="text"  value="<?= $selected_family['hsplatka'] ?>"
+                                data-form-input disabled>
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+            <div class="main-details">
+                    <p><strong>Zůstatek:</strong> 
                         <?= $selected_family['cashflow'] ?>
                     </p>
-                    <p>Dluh: <?=$selected_family['dluh']?></p>
+                    <p> <strong>Dluh:</strong>   <?=$selected_family['dluh']?></p>
                 </div>
-                <p>Pro úpravu přepište údaje a klikněte na tlačitko</p>
                 <div class="recommended">
+                    <div> <strong>Doporučení:</strong> </div>
                     <?php if ($selected_family['cashflow'] > (6 * ($selected_family['plat_1'] + $selected_family['plat_2']))): ?>
 
                         <p>Máš velkou rezervu :) investuj do kapitálových trhů</p>
@@ -38,109 +140,11 @@ if (isset($_POST['edit_id'])):
                         <p>Zvedni příjem</p>
                     <?php endif ?>
                 </div>
-
-                <div class="grid">
-                    <label for="name">Jméno rodiny</label>
-                    <input name="name" class="edit1" <?= $selected_family['name'] ?> data-form-input disabled>
-                    <!-- Prijmy -->
-                    <div class="clmn">
-                        <div class="form-group">
-                            <label for="plat">1.plat (měsíčně):</label>
-                            <input name="plat" type="text" class="edit" value="<?= $selected_family['plat_1'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="2plat">2.plat (měsíčně):</label>
-                            <input name="2plat" type="text" class="edit" value="<?= $selected_family['plat_2'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="jedno_prijem">Jednorázové přijmy:</label>
-                            <input name="jedno_prijem" type="text" class="edit"
-                                value="<?= $selected_family['jedno_prijem'] ?>" data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="investice">Investice:</label>
-                            <input name="investice" type="text" class="edit" value="<?= $selected_family['investice'] ?>"
-                                data-form-input disabled>
-                        </div>
-                    </div>
-                    <!-- Vydaje -->
-                    <div class="clmn">
-                        <div class="form-group">
-                            <label for="najem">Nájem/Daň(měsíčně):</label>
-                            <input name="najem" type="text" class="edit" value="<?= $selected_family['najem'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="inkaso">Voda+Elektřina(za měsíc):</label>
-                            <input name="inkaso" type="text" class="edit" value="<?= $selected_family['inkaso'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="jidlo">Jídlo:</label>
-                            <input name="jidlo" type="text" class="edit" value="<?= $selected_family['jidlo'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="telekomunikace">Telekomunikace:</label>
-                            <input name="telekomunikace" type="text" class="edit"
-                                value="<?= $selected_family['telekomunikace'] ?>" data-form-input disabled>
-                        </div>
-                    </div>
-                    <!-- Vydaje zbytne -->
-                    <div class="clmn">
-                        <div class="form-group">
-                            <label for="konicky">Koníčky:</label>
-                            <input name="konicky" type="text" class="edit" value="<?= $selected_family['konicky'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="vylety">Výlet+zábava..:</label>
-                            <input name="vylety" type="text" class="edit" value="<?= $selected_family['vylety'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="vzdelani">Vzdělání:</label>
-                            <input name="vzdelani" type="text" class="edit" value="<?= $selected_family['vzdelani'] ?>"
-                                data-form-input disabled>
-                        </div>
-
-                    </div>
-                    <!-- Dluhy -->
-                    <div class="clmn">
-                        <div class="form-group">
-                            <label for="kcelkem">Hypotéka:</label>
-                            <input name="kcelkem" type="text" class="edit" value="<?= $selected_family['kcelkem'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="ksplatka">Splátka hypo:</label>
-                            <input name="ksplatka" type="text" class="edit" value="<?= $selected_family['ksplatka'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="hcelkem">Kreditka:</label>
-                            <input name="hcelkem" type="text" class="edit" value="<?= $selected_family['hcelkem'] ?>"
-                                data-form-input disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="hsplatkay">Splátka kreditky:</label>
-                            <input name="hsplatka" type="text" class="edit" value="<?= $selected_family['hsplatka'] ?>"
-                                data-form-input disabled>
-                        </div>
-                    </div>
-
-                </div>
-            </form>
             <div class="button-group">
             <button class="button" data-enable-button>Upravit</button>
             <button class="button" onclick="location.reload()">Zavřít</button>
             <button class="button" onclick="Delete_Family(<?= $selected_family['ID'] ?>)">Smazat</button>
             </div>
-            
-
-            <br><br>
         </div>
 
 
